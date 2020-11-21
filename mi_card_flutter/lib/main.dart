c20e33610f176f54a8aedaadf3add164e0ea97a5
Tommy Shi <tommyshi19991130@gmail.com>
2020-11-21 14:34:18 +1300

    [MiCard] Add title and custom fonts

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -20,8 +20,19 @@ class MyApp extends StatelessWidget {
               Text(
                 'Tommy Shi',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'SOFTWARE ENGINEERING STUDENT',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal[100],
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                 ),
               ),
