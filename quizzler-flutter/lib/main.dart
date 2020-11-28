55840befeead408a6cc572b787f416d7d86cba97
Tommy Shi <tommyshi19991130@gmail.com>
2020-11-29 11:55:10 +1300

    [Quizzler] Check user answers and print to console

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -33,6 +33,8 @@ class _QuizPageState extends State<QuizPage> {
     'A slug\'s blood is green.'
   ];
 
+  List<bool> answers = [false, true, true];
+
   int questionNumber = 0;
 
   @override
@@ -72,11 +74,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == true) {
+                  print('user got it right');
+                } else {
+                  print('wrong');
+                }
+
                 setState(() {
                   questionNumber++;
-                  if (questionNumber >= 3) {
-                    questionNumber = 0;
-                  }
                 });
               },
             ),
@@ -96,11 +102,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == false) {
+                  print('user got it right');
+                } else {
+                  print('wrong');
+                }
+
                 setState(() {
                   questionNumber++;
-                  if (questionNumber >= 3) {
-                    questionNumber = 0;
-                  }
                 });
               },
             ),
