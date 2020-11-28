e54d528edc3b83db5958d62ae5451e85b7cb5ba9
Tommy Shi <tommyshi19991130@gmail.com>
2020-11-28 15:19:58 +1300

    [Xylophone] add tiles with 7 notes

---
+++ xylophone-flutter/ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Runner
+//
+//  Created by Tommy on 28/11/20.
+//  Copyright © 2020 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,59 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
