4753624e2b43a456ba49f0b4746dfeaa89f518a8
Tommy Shi <tommyshi19991130@gmail.com>
2021-01-02 23:28:35 +1300

    [Todoey] Use provider to check tasks off

--- todoey_flutter/lib/screens/add_task_screen.dart
+++ todoey_flutter/lib/screens/add_task_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todoey_flutter/models/task.dart';
 import 'package:todoey_flutter/models/task_data.dart';
 
 class AddTaskScreen extends StatelessWidget {
